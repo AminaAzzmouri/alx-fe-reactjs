be2ddbb4b6c1749b0824808d9c5a18764255dc5e
Amina Azzmouri <azzmouriamina@gmail.com>
2025-07-19 16:28:11 +0100

    Moved components to src/components path

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Services from './Pages/Services';
-import Contact from './Pages/Contact';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 
 function App() {
