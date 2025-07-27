343e6ad5626e9a5d4d66b00f12cc2d474db2f713
Amina Azzmouri <azzmouriamina@gmail.com>
2025-07-27 18:23:42 +0100

    fix: include BrowserRouter in App.jsx to satisfy auto-checker

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,24 +1,24 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
-
-
 function App() {
   return (
-    <div>
-      <h1><Link to="/">Recipe Sharing App</Link></h1>
-      <Routes>
-        <Route path="/" element={
-          <>
-            <AddRecipeForm />
-            <RecipeList />
-          </>
-        } />
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1><Link to="/">Recipe Sharing App</Link></h1>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          } />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
